d47634bdc9a30a0a1e6bcf78e5b98fb6234d8bb7
arnima12 <tasnimanzum1234@gmail.com>
2024-02-21 18:08:46 +0600

    attempt selected

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function showPrompt() {
     const randomNumber = Math.ceil(Math.random() * max);
     console.log(randomNumber)
     let attempts = 0;
+    let maxAttempts = 3;
     while(attempts<3){
         const guessNumber = prompt("Guess the number between 1-10 :")
     const number = parseInt(guessNumber)
@@ -11,9 +12,9 @@ function showPrompt() {
     }else{
     console.log(number)
         if(number < randomNumber){
-            alert("Correct answer is greater!!")
+            alert(`Correct answer is greater!! You have left ${maxAttempts-attempts-1} attempts`)
         }else if(number > randomNumber){
-            alert("Correct answer is smaller!!")
+            alert(`Correct answer is smaller!! You have left ${maxAttempts-attempts-1} attempts`)
         }else if(number === randomNumber){
             alert("You won");
             return;
